fd44b250c1a4df5bf95cb36a4a5163b3c522611f
jswo3901 <jswo3901@gmail.com>
2018-02-14 22:41:14 +0900

    박스추가

--- src/Home/Profile/Pro1Page.js
+++ src/Home/Profile/Pro1Page.js
@@ -1,9 +1,37 @@
 import React, { Component } from 'react';
-
+import './Pro1Page.css'
+//http://divtable.com/generator/
 class Pro1Page extends Component {
   render() {
     return (
-      <div> 프로1 소개내용 </div>
+      <div className="test">
+
+        <div class="divTable infoTable">
+          <div class="divTableBody">
+            <div class="divTableRow">
+              <div class="divTableCell">업소명 : 나비</div>
+              <div class="divTableCell">모집업종</div>
+            </div>
+            <div class="divTableRow">
+              <div class="divTableCell">담당자</div>
+              <div class="divTableCell">주소</div>
+            </div>
+            <div class="divTableRow">
+              <div class="divTableCell">연락처</div>
+              <div class="divTableCell">cell2_3</div>
+            </div>
+            <div class="divTableRow">
+              <div class="divTableCell">cell1_4</div>
+              <div class="divTableCell">cell2_4</div>
+            </div>
+            <div class="divTableRow">
+              <div class="divTableCell">cell1_5</div>
+              <div class="divTableCell">cell2_5</div>
+            </div>
+          </div>
+        </div>
+
+      </div>
     )
   }
 }
--- src/Home/index.js
+++ src/Home/index.js
@@ -121,7 +121,7 @@ const Home = () => {
               </b>
             </div>
           </div>  
-
+          
         </div>
     )
 }
