6fe26be271c36547d61e4917474c3c03730b04d3
jswo3901 <jswo3901@gmail.com>
2018-02-22 00:31:02 +0900

    메인 얼추 완성

--- src/Home/MainTop.js
+++ src/Home/MainTop.js
@@ -6,6 +6,8 @@ import {
 } from 'react-router-dom'
 import './MainTop.css'
 import main_top from './main_top.jpg'
+import Login from './login.jpg'
+
 class MainTop extends Component {
   render() {
     return (
@@ -22,13 +24,61 @@ class MainTop extends Component {
                 <ul id="PrimaryMenu">
                     <li className="list01">
                         <p className="titleSub">
-                            지역별 알바
+                            오늘의 추천 알바
                         </p>
                         <p className="menu">
-                        논현
+                          <img src="http://via.placeholder.com/158x139" />
                         </p>
                     </li>
+                    <li className="list02">
+                        <p className="titleSub">
+                            당일 알바
+                        </p>
+                        <p className="menu">
+                          <img src="http://via.placeholder.com/158x139" />
+                        </p>
+                    </li>
+                    <li className="list03">
+                        <p className="titleSub">
+                            인기 알바
+                        </p>
+                        <p className="menu">
+                          <img src="http://via.placeholder.com/158x139" />
+                        </p>
+                    </li>
+                    
                 </ul>
+                <div id="PrimaryMenu2">
+                  <img src="http://via.placeholder.com/198x169" />
+                </div>
+
+                <div id="PrimaryLoginMenu3">
+                  <div className="radio">
+                    <div className="first_radio">
+                      <input type="radio" name="contact" value="pri"/>
+                        <span>개인회원</span>
+                    </div>
+                    <div className="second_radio">
+                      <input clasName="second_radio1" type="radio" name="contact" value="com"/>
+                        <span>기업회원</span>
+                    </div>
+                  </div>
+                  <div className="input_id">
+                    <div>
+                      <input placeholder="ID" size="15"/>
+                    </div>
+                    <div>
+                      <input placeholder="PASSWORD" size="15"/>
+                    </div>
+                  </div>
+                    <button className="login_button">로그인 </button>
+                </div>
+
+                <div id="PrimaryMenu4">
+                  <a>공지사항!!</a>
+                  당분간 회원가입이 정지되었습니다. 
+                </div>
+
               </div>
             </div>
     );
